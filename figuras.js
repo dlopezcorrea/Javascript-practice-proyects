97ca829be7a236765c7c3601bf08472ed82f0367
Diego Lopez Correa <72930512+dlopezcorrea@users.noreply.github.com>
2022-06-03 13:46:39 -0500

    Finished working on functions

--- figuras.js
+++ figuras.js
@@ -16,3 +16,20 @@ function perimetroTriangulo(lado1, lado2, base){
 function areaTriangulo(base, altura){
     return (base * altura)/2
 }
+
+//Codigo del circulo
+
+function diametroCirculo(radio){
+    return radio * 2
+}
+
+const PI = Math.PI;
+
+function perimetroCirculo(radio){
+   const diametro = diametroCirculo(radio);
+   return diametro * PI
+}
+
+function areaCirculo(radio){
+    return radio * radio * PI;
+}
