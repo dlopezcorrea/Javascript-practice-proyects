7e66a3de1fc058ce6458836ac79be966fb31fdf4
Diego Lopez Correa <72930512+dlopezcorrea@users.noreply.github.com>
2022-06-03 11:47:31 -0500

    Adding some js code

---
+++ figuras.js
@@ -0,0 +1,18 @@
+// Codigo del cuadrado
+function perimetroCuadrado(lado){
+    return lado * 4
+}
+
+function areaCuadrado(lado){
+    return lado * lado;
+}
+
+//Codigo del triangulo
+
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base, altura){
+    return (base * altura)/2
+}
